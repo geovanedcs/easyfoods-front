b24e4334758c259a3b5a129c86be8e6f9aba49df
geovane <geovane@alunos.utfpr.edu.br>
2019-12-03 16:52:14 -0300

    att config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {ComidaComponent} from './comida/comida.component';
+import {LoginComponent} from './login/login.component';
+import {ComidaFormComponent} from './comida-form/comida-form.component';
+import {LoginFormComponent} from './login-form/login-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'comida', component: ComidaComponent
+  },
+  {
+    path: 'comida/form', component: ComidaFormComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'login/form', component: LoginFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,50 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import {
+  ButtonModule,
+  CardModule, ConfirmationService,
+  ConfirmDialogModule,
+  MenuModule,
+  MessageModule, MessageService,
+  SidebarModule,
+  TableModule,
+  ToastModule,
+  TooltipModule
+} from 'primeng';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { ComidaComponent } from './comida/comida.component';
+import { LoginComponent } from './login/login.component';
+import { ComidaFormComponent } from './comida-form/comida-form.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ComidaComponent,
+    LoginComponent,
+    ComidaFormComponent,
+    LoginFormComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SidebarModule,
+    ButtonModule,
+    ToastModule,
+    MenuModule,
+    TooltipModule,
+    CardModule,
+    TableModule,
+    ConfirmDialogModule,
+    HttpClientModule,
+    MessageModule,
   ],
-  providers: [],
+  providers: [MessageService, ConfirmationService, SidebarModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
