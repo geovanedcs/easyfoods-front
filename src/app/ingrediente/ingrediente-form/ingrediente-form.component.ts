3175049e5e6475f054790fbe63bb6cfb06a6e5bf
geovane <geovane@alunos.utfpr.edu.br>
2019-12-04 10:24:40 -0300

    ingrediente form implement

--- src/app/ingrediente/ingrediente-form/ingrediente-form.component.ts
+++ src/app/ingrediente/ingrediente-form/ingrediente-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingrediente} from '../../model/ingrediente';
+import {ActivatedRoute, Router} from '@angular/router';
+import {IngredienteService} from '../../service/ingrediente.service';
+import {MessageService} from 'primeng';
 
 @Component({
   selector: 'app-ingrediente-form',
@@ -6,10 +10,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ingrediente-form.component.scss']
 })
 export class IngredienteFormComponent implements OnInit {
+  objeto: Ingrediente;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute,
+              private ingredienteService: IngredienteService,
+              private router: Router,
+              private messageService: MessageService) { }
 
   ngOnInit() {
+    this.activatedRoute.queryParamMap.subscribe(params => {
+      if (params.has('id')) {
+        this.ingredienteService.findOne(parseInt(params.get('id'))).subscribe(res => {
+          this.objeto = res;
+        });
+      } else {
+        this.resetaForm();
+      }
+    });
+  }
+
+  salvar(): void {
+    this.ingredienteService.save(this.objeto).subscribe(res => {
+      this.objeto = res;
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Salvo com sucesso!'
+      });
+      this.router.navigateByUrl('ingrediente');
+    }, erro => {
+      this.messageService.add({
+        severity: 'error',
+        summary: erro.error.message
+      });
+    });
+  }
+
+  private resetaForm(): void {
+    this.objeto = new Ingrediente();
+    this.objeto.ingrediente = '';
   }
 
 }
