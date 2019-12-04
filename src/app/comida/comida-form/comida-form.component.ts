39c7eb13a1dfb45b19bbfff088de84508a1dd8b5
andreluis2019 <al.colla@hotmail.com>
2019-12-04 15:30:06 -0300

    Ajustado multiselect

--- src/app/comida/comida-form/comida-form.component.ts
+++ src/app/comida/comida-form/comida-form.component.ts
@@ -4,6 +4,8 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {ComidaService} from '../../service/comida.service';
 import {MessageService} from 'primeng';
 import {Ingrediente} from '../../model/ingrediente';
+import {IngredienteService} from "../../service/ingrediente.service";
+import {ComidaIngrediente} from "../../model/comidaIngrediente";
 
 
 @Component({
@@ -14,17 +16,27 @@ import {Ingrediente} from '../../model/ingrediente';
 export class ComidaFormComponent implements OnInit {
   objeto: Comida;
   ingredientes: Ingrediente[];
+  selectedIngredientes: Ingrediente[] = [];
 
   constructor(private activatedRoute: ActivatedRoute,
               private comidaService: ComidaService,
+              private ingredienteService: IngredienteService,
               private router: Router,
-              private messageService: MessageService) { }
+              private messageService: MessageService) {
+    ingredienteService.findAll().subscribe( ingrediente => {
+      this.ingredientes = ingrediente;
+    });
+  }
 
   ngOnInit() {
     this.activatedRoute.queryParamMap.subscribe(params => {
       if (params.has('id')) {
         this.comidaService.findOne(parseInt(params.get('id'))).subscribe(res => {
           this.objeto = res;
+          this.selectedIngredientes = [];
+          for (const comidaIngrediente of this.objeto.ingredienteList) {
+            this.selectedIngredientes.push(comidaIngrediente.ingrediente);
+          }
         });
       } else {
         this.resetaForm();
@@ -33,6 +45,13 @@ export class ComidaFormComponent implements OnInit {
   }
 
   salvar(): void {
+    this.objeto.ingredienteList = [];
+    for (const ingrediente of this.selectedIngredientes) {
+      const comidaIngrediente = new ComidaIngrediente();
+      comidaIngrediente.ingrediente = ingrediente;
+      this.objeto.ingredienteList.push(comidaIngrediente);
+    }
+
     this.comidaService.save(this.objeto).subscribe(res => {
       this.objeto = res;
       this.messageService.add({
