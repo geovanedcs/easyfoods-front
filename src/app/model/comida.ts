41cd6440d1240e680e2765ad118bd330f8fb5506
andreluis2019 <al.colla@hotmail.com>
2019-12-04 15:29:36 -0300

    Criado model comidaIngrediente

--- src/app/model/comida.ts
+++ src/app/model/comida.ts
@@ -1,5 +1,8 @@
+import {ComidaIngrediente} from "./comidaIngrediente";
+
 export class Comida {
   id: number;
   comida: string;
   inativo: boolean;
+  ingredienteList: ComidaIngrediente[];
 }
---
+++ src/app/model/comidaIngrediente.ts
@@ -0,0 +1,8 @@
+import {Comida} from "./comida";
+import {Ingrediente} from "./ingrediente";
+
+export class ComidaIngrediente {
+  id: number;
+  comida: Comida;
+  ingrediente: Ingrediente;
+}
