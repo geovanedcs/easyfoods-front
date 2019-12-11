134b31d401dbb89b79b81c561825977e1f49ee6a
andreluis2019 <al.colla@hotmail.com>
2019-12-11 10:29:04 -0300

    Ajuste nas mensagens de campo requerido na tela de login

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,17 +3,17 @@ import {NgModule} from '@angular/core';
 import {HomeComponent} from './home.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
-import {VMessageModule} from '../vmessage/vmessage.module';
 import {SigninComponent} from '../auth/signin/signin.component';
 import {RouterModule} from '@angular/router';
+import {NgxMaskModule} from "ngx-mask";
 
 @NgModule({
   declarations: [SigninComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    VMessageModule,
-    RouterModule
+    RouterModule,
+    NgxMaskModule
   ],
   providers: [],
   bootstrap: [HomeComponent]
--- src/app/vmessage/vmessage.module.ts
+++
@@ -1,9 +0,0 @@
-import {NgModule} from "@angular/core";
-import {VMessageComponent} from "./vmessage.component";
-
-@NgModule({
-  declarations: [VMessageComponent],
-  exports: [VMessageComponent]
-})
-
-export class VMessageModule { }
