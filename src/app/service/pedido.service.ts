5791f028e4b0eed7107d2d9af1fe257f0c92c129
maidops@gmail.com <rvmp196994>
2019-12-12 09:41:04 -0300

    metodo pedido por cliente

--- src/app/service/pedido.service.ts
+++ src/app/service/pedido.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {BaseService} from './base.service';
 import {Pedido} from '../model/pedido';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +11,9 @@ export class PedidoService extends BaseService<Pedido> {
 
   constructor(protected http: HttpClient) {
     super(http, 'pedido');
+  }
+
+  findPedidoByCliente(id: number): Observable<Pedido[]> {
+    return this.http.get<Pedido[]>(`${this.getUrl()}/cliente?id=${id}`);
   }
 }
